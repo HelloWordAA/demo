e2ea13fddff28ef6991f67c3926bfd2e13473498
yan <yan@qq.com>
2020-12-06 23:32:55 +0800

    aa

--- shop_service/shop_service_goods/src/main/java/com/qf/dao/GoodsMapper.java
+++ shop_service/shop_service_goods/src/main/java/com/qf/dao/GoodsMapper.java
@@ -3,5 +3,5 @@ package com.qf.dao;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.qf.entity.Goods;
 //oo
-public interface GoodsMapper extends BaseMapper<Good> {
+public interface GoodsMapper extends BaseMapper<Goods> {
 }
